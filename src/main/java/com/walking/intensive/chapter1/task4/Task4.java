2bf6627b0442a985dc0661fee307f112fbcf226a
LVictor-SF <Livanov.v.a@ya.ru>
2024-11-21 01:29:32 +0300

    ver 1.0

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -27,7 +27,6 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
         double b = 0;
         double c = 0;
@@ -37,8 +36,41 @@ public class Task4 {
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        double d = b * b - (4 * a * c);
+        double x1 = 0;
+        double x2 = 0;
+
+        if (a == 0 & b == 0 & c == 0) {
+            return "Бесконечное множество решений";
+        }
+
+        if (a == 0 & b == 0) {
+            return "Некорректные входные данные";
+        }
+
+        if (a == 0) {
+            x1 = (0 - c) / b;
+            return "Количество решений: 1. Корень: " + (int) x1;
+
+        }
+
+        if (d < 0) {
+            return "Количество решений: 0" + ".";
+        }
+
+        if (d == 0) {
+            x1 = (0 - b) / (2 * a);
+            return "Количество решений: 1. Корень: " + (int) x1;
+        }
+
+        if (d > 0) {
+            x1 = (((0 - b) + Math.sqrt(d)) / (2 * a));
+            x2 = (((0 - b) - Math.sqrt(d)) / (2 * a));
+            if (x1 > x2) {
+                return "Количество решений: 2. Корни: " + (int) x1 + ";" + (int) x2;
+            }
+        }
+        return "Количество решений: 2. Корни: " + (int) x2 + ";" + (int) x1;
     }
 }
