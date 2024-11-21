f6178b85c5b90ff17d310f28f4fd020f1d0cb95d
LVictor-SF <Livanov.v.a@ya.ru>
2024-11-21 10:33:10 +0300

    ver 2.0 Переделал без if

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -25,19 +25,9 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Task3 {
     public static void main(String[] args) {
-
     }
 
     static boolean isLeap(int year) {
-
-        if (year % 4 != 0) {
-            return false;
-        } else {
-            if (year % 100 != 0) {
-                return true;
-            } else {
-                return year % 400 == 0;
-            }
-        }
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
     }
 }
